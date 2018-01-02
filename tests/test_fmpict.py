4d10a174ad88f0ebf6dcc36535e4b1b1f0e1afc9
Hiroki Iseri <goyoki@gmail.com>
2018-01-02 16:34:00 +0900

    support vscode

---
+++ tests/test_fmpict.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+import unittest
+
+class TestFMPict(unittest.TestCase):
+    def test_hoge(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
