8537bbfabaf6ea65cbce2a1669f1c436bfdaa082
Hiroki Iseri <goyoki@gmail.com>
2018-01-03 17:40:39 +0900

    fix for text insert

--- fmpict/fmpict.py
+++ fmpict/fmpict.py
@@ -13,17 +13,20 @@ class NodeType(object):
     VAILD_DATA = 3
     FACTOR = 4
     EXEC_OPTION = 5
+    HEADER = 6
+    SUB_MODEL_DEFINITIONS = 7
+    CONSTRAINT_DEFINITIONS = 8
 
 class FMCTMGenerator(object):
     """generates test condition from FreeMind's file"""
 
     def __init__(self):
-        self._clsf_dict = {}
-        self._pict_exec_option = ""
+        self._init_gendata()
     
     def _init_gendata(self):
         self._clsf_dict = {}
         self._pict_exec_option = ""
+        self._insert_text = {}
 
     def get_testcon(self):
         """return last class set"""
@@ -31,15 +34,23 @@ class FMCTMGenerator(object):
 
     @staticmethod
     def _get_node_type(node):
+        word_dict = {'[pict_exec_option]':NodeType.EXEC_OPTION,
+            '[sub_model_definitions]':NodeType.SUB_MODEL_DEFINITIONS,
+            '[constraint_definitions]':NodeType.CONSTRAINT_DEFINITIONS}
+
         if not 'TEXT' in node.attrib:
             return NodeType.NO_DATA
         if not node.attrib['TEXT']:
             return NodeType.NO_DATA
+        
         attrib_text = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
         if attrib_text[0] == '#':
             return NodeType.COMMENT
-        if '[pict_exec_option]' in attrib_text:
-            return NodeType.EXEC_OPTION
+
+        for key, value in word_dict.items():
+            if key in attrib_text:
+                return value
+
         if [x for x in node if x.attrib == {'BUILTIN': 'folder'}]:
             return NodeType.FACTOR
         return NodeType.VAILD_DATA
@@ -63,6 +74,17 @@ class FMCTMGenerator(object):
                     text_data = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
                     self._pict_exec_option = self._pict_exec_option + ' ' + text_data
         
+        if node_type == NodeType.HEADER or \
+            node_type == NodeType.CONSTRAINT_DEFINITIONS or \
+            node_type == NodeType.SUB_MODEL_DEFINITIONS:
+            for node in [y for y in list(parent) if 'TEXT' in y.attrib]:
+                child_node_type = self._get_node_type(node)
+                if child_node_type == NodeType.VAILD_DATA:
+                    text_data = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
+                    if node_type in self._insert_text:
+                        print('Warning:Duplicate Insert Text[%d]' % (node_type))
+                    self._insert_text[node_type] = text_data
+
         if node_type == NodeType.FACTOR:
             cf_text = parent.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
             class_list = []
@@ -81,15 +103,18 @@ class FMCTMGenerator(object):
         return self._clsf_dict
 
     @staticmethod
-    def _gen_pict_input_file(file_path, clsf_dict):
+    def _gen_pict_input_file(file_path, clsf_dict, insert_text_dict):
         try:
             with open(file_path, "w") as pict_input_file:
                 for key, classlist in clsf_dict.items():
                     line = key + ':' + ",".join(classlist) + '\n'
                     pict_input_file.write(line)
+                if NodeType.SUB_MODEL_DEFINITIONS in insert_text_dict:
+                    pict_input_file.write(insert_text_dict[NodeType.SUB_MODEL_DEFINITIONS])
+                if NodeType.CONSTRAINT_DEFINITIONS in insert_text_dict:
+                    pict_input_file.write(insert_text_dict[NodeType.CONSTRAINT_DEFINITIONS])
         except IOError:
             print('Error:pict file cannot be created')
-            raise
 
     @staticmethod
     def _print_testcondition(file_path, exec_option):
@@ -118,7 +143,9 @@ class FMCTMGenerator(object):
                 file_path = pictfile_path
             else:
                 file_path = "temp.csv"
-            self._gen_pict_input_file(file_path, self._clsf_dict)
+
+            self._gen_pict_input_file(file_path, self._clsf_dict, self._insert_text)
+
             if not only_gen_pictfile:
                 self._print_testcondition(file_path, self._pict_exec_option)
             if not save_pictfile:
