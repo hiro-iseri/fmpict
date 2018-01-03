fbd99f74e33a07f6effb5c298bc81edd3ccfb2bb
Hiroki Iseri <goyoki@gmail.com>
2018-01-04 00:30:51 +0900

    add link_def(TBD)

--- fmpict/fmpict.py
+++ fmpict/fmpict.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-"""Simple classification tree tool using FreeMind and PICT"""
+"""pairwise testing tool using FreeMind and PICT"""
 
 import xml.etree.ElementTree as ET
 import subprocess
@@ -8,6 +8,7 @@ import sys
 import os
 
 class NodeType(object):
+    """type of Freeminde node"""
     NO_DATA = 1
     COMMENT = 2
     VAILD_DATA = 3
@@ -16,37 +17,52 @@ class NodeType(object):
     HEADER = 6
     SUB_MODEL_DEFINITIONS = 7
     CONSTRAINT_DEFINITIONS = 8
+    LINK_DEF = 9
+    LINK_REFER = 10
+    
+    @staticmethod
+    def is_valid_data_node(node_type):
+        return node_type == NodeType.VAILD_DATA or node_type == NodeType.LINK_REFER
 
 class FMCTMGenerator(object):
-    """generates test condition from FreeMind's file"""
+    """generates test condition from FreeMind file"""
 
     def __init__(self):
-        self._init_gendata()
-    
-    def _init_gendata(self):
         self._clsf_dict = {}
         self._pict_exec_option = ""
         self._insert_text = {}
+        self._link_def = {}
+    
+    def _init_gendata(self):
+        """initialize analysis data"""
+        self._clsf_dict = {}
+        self._pict_exec_option = ""
+        self._insert_text = {}
+        self._link_def = {}
 
     def get_testcon(self):
-        """return last class set"""
+        """return last pict data"""
         return self._clsf_dict
 
     @staticmethod
     def _get_node_type(node):
-        word_dict = {'[pict_exec_option]':NodeType.EXEC_OPTION,
-            '[sub_model_definitions]':NodeType.SUB_MODEL_DEFINITIONS,
-            '[constraint_definitions]':NodeType.CONSTRAINT_DEFINITIONS}
-
+        """get node type from node.attrib"""
         if not 'TEXT' in node.attrib:
             return NodeType.NO_DATA
         if not node.attrib['TEXT']:
             return NodeType.NO_DATA
-        
-        attrib_text = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
-        if attrib_text[0] == '#':
-            return NodeType.COMMENT
 
+        attrib_text = FMCTMGenerator._get_text_str(node)
+
+        prefix_dict = {'#':NodeType.COMMENT, '<':NodeType.LINK_DEF,
+                       '>':NodeType.LINK_REFER, '@':NodeType.FACTOR}
+        for key, value in prefix_dict.items():
+            if attrib_text[0] == key:
+                return value
+
+        word_dict = {'[pict_exec_option]':NodeType.EXEC_OPTION,
+                     '[sub_model_definitions]':NodeType.SUB_MODEL_DEFINITIONS,
+                     '[constraint_definitions]':NodeType.CONSTRAINT_DEFINITIONS}
         for key, value in word_dict.items():
             if key in attrib_text:
                 return value
@@ -55,14 +71,13 @@ class FMCTMGenerator(object):
             return NodeType.FACTOR
         return NodeType.VAILD_DATA
 
-    def _get_testcon_from_node(self, parent):
-        """reads class set from freemind's node"""
-        node_type = self._get_node_type(parent)
+    @staticmethod
+    def _get_text_str(node):
+        return node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
 
-        if node_type == NodeType.NO_DATA:
-            for node in list(parent):
-                self._get_testcon_from_node(node)
-            return self._clsf_dict
+    def _get_testcon_from_node(self, parent):
+        """reads class set from freemind node"""
+        node_type = self._get_node_type(parent)
 
         if node_type == NodeType.COMMENT:
             return self._clsf_dict
@@ -70,8 +85,8 @@ class FMCTMGenerator(object):
         if node_type == NodeType.EXEC_OPTION:
             for node in [y for y in list(parent) if 'TEXT' in y.attrib]:
                 child_node_type = self._get_node_type(node)
-                if child_node_type == NodeType.VAILD_DATA:
-                    text_data = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
+                if NodeType.is_valid_data_node(child_node_type):
+                    text_data = self._get_text_str(node)
                     self._pict_exec_option = self._pict_exec_option + ' ' + text_data
         
         if node_type == NodeType.HEADER or \
@@ -79,28 +94,44 @@ class FMCTMGenerator(object):
             node_type == NodeType.SUB_MODEL_DEFINITIONS:
             for node in [y for y in list(parent) if 'TEXT' in y.attrib]:
                 child_node_type = self._get_node_type(node)
-                if child_node_type == NodeType.VAILD_DATA:
-                    text_data = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
+                if NodeType.is_valid_data_node(child_node_type):
+                    text_data = self._get_text_str(node)
                     if node_type in self._insert_text:
                         print('Warning:Duplicate Insert Text[%d]' % (node_type))
                     self._insert_text[node_type] = text_data
 
         if node_type == NodeType.FACTOR:
-            cf_text = parent.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
+            cf_text = self._get_text_str(parent)
             class_list = []
             for node in [y for y in list(parent) if 'TEXT' in y.attrib]:
                 child_node_type = self._get_node_type(node)
-                if child_node_type == NodeType.VAILD_DATA:
-                    text_data = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
+                if NodeType.is_valid_data_node(child_node_type):
+                    text_data = self._get_text_str(node)
                     class_list.append(text_data)
             if class_list:
                 self._clsf_dict[cf_text] = class_list
 
-            return self._clsf_dict
+        if node_type == NodeType.LINK_DEF:
+            cf_text = self._get_text_str(parent)
+            class_list = []
+            for node in [y for y in list(parent) if 'TEXT' in y.attrib]:
+                child_node_type = self._get_node_type(node)
+                if NodeType.is_valid_data_node(child_node_type):
+                    text_data = self._get_text_str(node)
+                    class_list.append(text_data)
+            if class_list:
+                self._link_def[cf_text] = class_list
 
+            return self._link_def
+
+        # NodeType.NO_DATA or non type
         for node in list(parent):
             self._get_testcon_from_node(node)
         return self._clsf_dict
+
+    def _replace_link_def(self):
+        for key, value in self._clsf_dict.items():
+            pass
 
     @staticmethod
     def _gen_pict_input_file(file_path, clsf_dict, insert_text_dict):
@@ -128,14 +159,15 @@ class FMCTMGenerator(object):
 
     def generate(self, input_file, only_gen_pictfile=False, save_pictfile=False, pictfile_path=""):
         """generates test condition from FreeMind file"""
-
         self._init_gendata()
         try:
             cls_tree = ET.parse(input_file)
         except ET.ParseError:
             print('"%s" is invalid format' % input_file)
             raise
+
         self._get_testcon_from_node(cls_tree.getroot())
+
         if not self._clsf_dict:
             print("Error:FreeMind file is invalid")
         else:
