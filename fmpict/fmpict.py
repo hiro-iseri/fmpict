229205c52590e7b550711f97b6bbef2a38e1a9ee
Hiroki Iseri <goyoki@gmail.com>
2018-01-03 14:44:33 +0900

    support for pictfile

--- fmpict/fmpict.py
+++ fmpict/fmpict.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 import subprocess
 import argparse
 import sys
+import os
 
 class NodeType(object):
     NO_DATA = 1
@@ -13,17 +14,16 @@ class NodeType(object):
     FACTOR = 4
     EXEC_OPTION = 5
 
-
 class FMCTMGenerator(object):
     """generates test condition from FreeMind's file"""
 
     def __init__(self):
         self._clsf_dict = {}
-        self._pict_exec_option = {}
+        self._pict_exec_option = ""
     
     def _init_gendata(self):
         self._clsf_dict = {}
-        self._pict_exec_option = {}
+        self._pict_exec_option = ""
 
     def get_testcon(self):
         """return last class set"""
@@ -61,10 +61,7 @@ class FMCTMGenerator(object):
                 child_node_type = self._get_node_type(node)
                 if child_node_type == NodeType.VAILD_DATA:
                     text_data = node.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
-                    if not self._pict_exec_option:
-                        self._pict_exec_option = text_data
-                    else:
-                        self._pict_exec_option = self._pict_exec_option + ' ' + text_data
+                    self._pict_exec_option = self._pict_exec_option + ' ' + text_data
         
         if node_type == NodeType.FACTOR:
             cf_text = parent.attrib['TEXT'].encode(sys.stdout.encoding).decode(sys.stdout.encoding)
@@ -84,19 +81,27 @@ class FMCTMGenerator(object):
         return self._clsf_dict
 
     @staticmethod
-    def _print_testcondition(clsf_dict, exec_option):
-        """output class set to stdout"""
+    def _gen_pict_input_file(file_path, clsf_dict):
         try:
-            with open("temp.csv", "w") as pict_input_file:
+            with open(file_path, "w") as pict_input_file:
                 for key, classlist in clsf_dict.items():
                     line = key + ':' + ",".join(classlist) + '\n'
                     pict_input_file.write(line)
         except IOError:
-            print('pict file cannot be created')
+            print('Error:pict file cannot be created')
             raise
-        subprocess.Popen("pict temp.csv %s" % (exec_option), shell=True)
 
-    def generate(self, input_file):
+    @staticmethod
+    def _print_testcondition(file_path, exec_option):
+        """output class set to stdout"""
+        command = "pict %s%s" % (file_path, exec_option)
+        prc = subprocess.Popen(command.split())
+        if prc:
+            prc.wait()
+        else:
+            print('Error:cannot run pict')
+
+    def generate(self, input_file, only_gen_pictfile=False, save_pictfile=False, pictfile_path=""):
         """generates test condition from FreeMind file"""
 
         self._init_gendata()
@@ -109,20 +114,30 @@ class FMCTMGenerator(object):
         if not self._clsf_dict:
             print("Error:FreeMind file is invalid")
         else:
-            self._print_testcondition(self._clsf_dict, self._pict_exec_option)
+            if pictfile_path:
+                file_path = pictfile_path
+            else:
+                file_path = "temp.csv"
+            self._gen_pict_input_file(file_path, self._clsf_dict)
+            if not only_gen_pictfile:
+                self._print_testcondition(file_path, self._pict_exec_option)
+            if not save_pictfile:
+                os.remove(file_path)
 
 def _get_parser():
     """create parser"""
     parser = argparse.ArgumentParser(
         description='This script is test tool generates test cases from freemind.')
     parser.add_argument('freemind_file_path', help='*.mm input file', type=argparse.FileType('r'))
-    parser.add_argument('-g', '--genparamlist', dest='generates param list file', help='run without calling pict')
+    parser.add_argument('-p', '--pict_file_path', help='pict file path', type=str)
+    parser.add_argument('-g', '--genparamlist', help='run without calling pict', action="store_true")
+    parser.add_argument('-s', '--savepictfile', help='save pict file', action="store_true")
     return parser
 
 def main():
-    parser = _get_parser()
+    args = _get_parser().parse_args()
     gen = FMCTMGenerator()
-    gen.generate(parser.parse_args().freemind_file_path)
+    gen.generate(args.freemind_file_path, args.genparamlist, args.savepictfile, args.pict_file_path)
 
 if __name__ == '__main__':
     main()
