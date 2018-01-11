01c6b380a1ff209529de1a0e991a56e66fcc00f6
Hiroki Iseri <goyoki@gmail.com>
2018-01-12 02:14:36 +0900

    add unitttest

--- tests/unittest_fmpict.py
+++ tests/unittest_fmpict.py
@@ -14,16 +14,19 @@ class UnitTestFMPict(unittest.TestCase):
     def setUp(self):
         self._target_dir_path = root_path +  '\\fmpict\\'
         self._work_dir_path = root_path +  '\\tests\worktmp\\'
-        self._test_dir_path = root_path +  '\\tests\\fm_sample\\'
+        self._test_dir_path = root_path +  '/tests/fm_sample/'
         if os.path.exists(self._work_dir_path):
             shutil.rmtree(self._work_dir_path)
         os.mkdir(self._work_dir_path)
     
     def testRunningPict(self):
+        expect = {u'A': [u'1', u'2', u'3'], u'B': [u'x', u'y', u'z']}
         gen = FMCTMGenerator()
         result = gen.get_testconditions_from_fmfile(self._test_dir_path + 'simple.mm')
-        print result
-        self.assertEqual(True, True)
+
+        for key, value in result.items():
+            self.assertTrue(expect.has_key(key))
+            self.assertEqual(set(value), set(expect[key]))
         
 if __name__ == '__main__':
     unittest.main()
