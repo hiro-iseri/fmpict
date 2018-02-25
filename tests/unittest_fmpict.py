8be233916980dc340d854fbf0a905d2314836180
Hiroki Iseri <goyoki@gmail.com>
2018-02-25 19:21:31 +0900

    add test

--- tests/unittest_fmpict.py
+++ tests/unittest_fmpict.py
@@ -7,26 +7,40 @@ import shutil
 
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, root_path)
-from fmpict.fmpict import FMCTMGenerator
-
+from fmpict import fmpict
 
 class UnitTestFMPict(unittest.TestCase):
-    def setUp(self):
-        self._target_dir_path = root_path +  '/fmpict/'
-        self._work_dir_path = root_path +  '/tests/worktmp/'
-        self._test_dir_path = root_path +  '/tests/fm_sample/'
-        if os.path.exists(self._work_dir_path):
-            shutil.rmtree(self._work_dir_path)
-        os.mkdir(self._work_dir_path)
+    @classmethod
+    def setUpClass(cls):
+        cls._target_dir_path = root_path +  '/fmpict/'
+        cls._work_dir_path = root_path +  '/tests/worktmp/'
+        cls._test_dir_path = root_path +  '/tests/fm_sample/'
+        if os.path.exists(cls._work_dir_path):
+            shutil.rmtree(cls._work_dir_path)
+        os.mkdir(cls._work_dir_path)
 
     
-    def testRunningPict(self):
+    def _testRunningPict(self):
         expect = {u'A': [u'1', u'2', u'3'], u'B': [u'x', u'y', u'z']}
+        result = FMCTMGenerator._init_gendata()
         result = FMCTMGenerator.get_testconditions_from_fmfile(self._test_dir_path + 'simple.mm')
 
         for key, value in result.items():
             self.assertTrue(key in expect)
             self.assertEqual(set(value), set(expect[key]))
-        
+
+
+    def testFmPict_saveTestConditionFile(self):
+        fmpict.run(self._test_dir_path + 'complex_input.mm', False, True, self._work_dir_path + 'savecon.txt', '')
+        self.assertTrue(os.path.exists(self._work_dir_path + 'savecon.txt'))
+
+    def testFmPict_notSaveTestConditionFile(self):
+        fmpict.run(self._test_dir_path + 'complex_input.mm', False, False, self._work_dir_path + 'notsavecon.txt', '')
+        self.assertFalse(os.path.exists(self._work_dir_path + 'notsavecon.txt'))
+
+    def testFmPict_notRunPict(self):
+        fmpict.run(self._test_dir_path + 'complex_input.mm', True, True, self._work_dir_path + 'notsavecon.txt', '')
+        self.assertFalse(os.path.exists(self._work_dir_path + 'savecon.txt'))
+
 if __name__ == '__main__':
     unittest.main()
