5eabb1e2e2ebd70dab80bd8f2b7b6398d3da844a
Hiroki Iseri <goyoki@gmail.com>
2018-02-25 23:45:25 +0900

    add test

--- tests/unittest_fmpict.py
+++ tests/unittest_fmpict.py
@@ -28,19 +28,26 @@ class UnitTestFMPict(unittest.TestCase):
         for key, value in result.items():
             self.assertTrue(key in expect)
             self.assertEqual(set(value), set(expect[key]))
+    
+    def test_get_testconditions_simple(self):
+        expect = {u'A': [u'1', u'2', u'3'], u'B': [u'x', u'y', u'z']}
+        result = fmpict.get_testconditions(self._test_dir_path + 'simple.mm')
+        self.assertTrue(result)
+        for key, value in result.items():
+            self.assertTrue(key in expect)
+            self.assertEqual(set(value), set(expect[key]))        
 
-
-    def testFmPict_saveTestConditionFile(self):
+    def test_run_saveTestConditionFile(self):
         fmpict.run(self._test_dir_path + 'complex_input.mm', False, True, self._work_dir_path + 'savecon.txt', '')
         self.assertTrue(os.path.exists(self._work_dir_path + 'savecon.txt'))
 
-    def testFmPict_notSaveTestConditionFile(self):
-        fmpict.run(self._test_dir_path + 'complex_input.mm', False, False, self._work_dir_path + 'notsavecon.txt', '')
+    def test_run_notSaveTestConditionFile(self):
+        fmpict.run(self._test_dir_path + 'complex_input.mm', False, False, self._work_dir_path + 'notsavecon.txt', '[dummy]')
         self.assertFalse(os.path.exists(self._work_dir_path + 'notsavecon.txt'))
 
-    def testFmPict_notRunPict(self):
-        fmpict.run(self._test_dir_path + 'complex_input.mm', True, True, self._work_dir_path + 'notsavecon.txt', '')
-        self.assertFalse(os.path.exists(self._work_dir_path + 'savecon.txt'))
+    def test_run_notRunPict(self):
+        fmpict.run(self._test_dir_path + 'complex_input.mm', True, True, self._work_dir_path + 'notrunpict.txt', '')
+        self.assertTrue(os.path.exists(self._work_dir_path + 'notrunpict.txt'))
 
 if __name__ == '__main__':
     unittest.main()
